c8fcbcb5a1bf4d06c94f586b5b5fd89e376e4227
TaylorDarneille <tmdarneille@gmail.com>
2022-02-15 14:34:51 -0800

    photos and clearing list

--- script.js
+++ script.js
@@ -5,19 +5,32 @@ const fetchUsers = (n) =>{
     fetch(endpoint) // returns a fetch object that contains the data
     .then(fetchObj=>fetchObj.json()) // unwrap the obj to get the json data
     .then(jsonData=>{
+        console.log(jsonData.results)
         addUsersToDom(jsonData.results)
     })
     .catch(err=>console.log('There was an error fetching data:', err))
 }
 
 const addUsersToDom = (apiResults) => {
+    let peopleList = document.querySelector('#peopleList')
+    // clear the unordered list if there are any results
+    while(peopleList.firstChild){
+        peopleList.firstChild.remove()
+    }
+
     for(let i=0; i<apiResults.length; i++) {
         // create a new li
         let newUser = document.createElement('li')
+        // create a new img
+        let newImg = document.createElement('img')
         // add the name of the user to the li
         newUser.textContent = apiResults[i].name.first + " " + apiResults[i].name.last
+        // add the source to the image
+        newImg.src = apiResults[i].picture.large
         // append the users to the ul
-        document.querySelector('#peopleList').appendChild(newUser)
+        peopleList.appendChild(newUser)
+        // append the image to the ul
+        peopleList.appendChild(newImg)
     }
 }
 
