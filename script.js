67492b205d42ef1d52558c5cfdc4a1beaab154d6
TaylorDarneille <tmdarneille@gmail.com>
2022-02-15 14:06:13 -0800

    loading user list

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    
+const fetchUsers = (n) =>{
     // make fetch happen
-    const endpoint = 'http://www.reddit.comq=kittens'
+    const endpoint = `https://randomuser.me/api/?results=${n}`
     // fetch requires the api url as an argument
     fetch(endpoint) // returns a fetch object that contains the data
     .then(fetchObj=>fetchObj.json()) // unwrap the obj to get the json data
     .then(jsonData=>{
-        console.log("here is the data: \n", jsonData)
+        addUsersToDom(jsonData.results)
     })
-    .catch(err=>console.log('DONUTDONUTDONUT', err))
+    .catch(err=>console.log('There was an error fetching data:', err))
+}
 
-    console.log('WHEN WILL WE SEE THIS?')
+const addUsersToDom = (apiResults) => {
+    for(let i=0; i<apiResults.length; i++) {
+        // create a new li
+        let newUser = document.createElement('li')
+        // add the name of the user to the li
+        newUser.textContent = apiResults[i].name.first + " " + apiResults[i].name.last
+        // append the users to the ul
+        document.querySelector('#peopleList').appendChild(newUser)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let numUsersToGet // declare variable
+
+    // add a submission event listener to form
+    document.querySelector('form').addEventListener('submit', e=>{
+        // prevent page refresh
+        e.preventDefault()
+        // assign value from input box to numUsersToGet
+        numUsersToGet = document.getElementById('numUsers').value
+        // fetch the users!
+        fetchUsers(numUsersToGet)
+    })
+
 })
